0498f318e42bc84c7803f3536e02b287f2544d9e
YasirNisar <yasirnisar66@gmail.com>
2025-01-19 18:29:14 +0530

    Muddu

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: ["http://localhost:5173", "https://zakat-care.vercel.app"],
+    origin: ["*"],
     methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE", "OPTIONS"],
 
     optionsSuccessStatus: 204,
